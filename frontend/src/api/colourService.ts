9dbea1848a9727ae66daf45156ba1f258f765535
Max A <thenasher@hotmail.com>
2025-02-11 01:06:50 +1100

    Rewrites frontend to use Next for clearer API handling and no CORS errors

--- frontend/src/api/colourService.ts
+++
@@ -1,8 +0,0 @@
-import { Colour } from "../../../backend/src/types/colourTypes";
-
-export const fetchColours = async (): Promise<Colour[]> => {
-    const response: Response = await fetch("http://localhost:3000/colours");
-    if (!response.ok) throw new Error("You cannot fetch colours at this time, please try again later");
-    const colourData: Colour[] = await response.json();
-    return colourData;
-  };
---
+++ frontend/src/app/api/getColours.ts
@@ -0,0 +1,6 @@
+import { NextResponse } from "next/server";
+import { getRandomColourSwatch } from "../../../../backend/src/services/colourService"; 
+
+export async function GET() {
+  return NextResponse.json(getRandomColourSwatch());
+}
